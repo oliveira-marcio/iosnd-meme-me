8a5546b0b038b1552ea52ef4e2bb91af79711f6a
oliveira-marcio <emaildomso@gmail.com>
2020-08-06 12:47:57 +0100

    feat: Display selected imaged

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
@@ -18,7 +18,20 @@ class ViewController: UIViewController {
 
     @IBAction func pickAnImage(_ sender: Any) {
         let pickController = UIImagePickerController()
+        pickController.delegate = self
         present(pickController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imagePickerView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("canceled")
+        dismiss(animated: true, completion: nil)
     }
     
 }
