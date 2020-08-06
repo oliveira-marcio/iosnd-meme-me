11847ee7241f83a53c1c2c19b2231f29d2aac8df
oliveira-marcio <emaildomso@gmail.com>
2020-08-06 15:54:42 +0100

    feat: Add pick image from camera

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,30 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var albumItem: UIBarButtonItem!
+    @IBOutlet weak var cameraItem: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.albumItem.isEnabled = UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        self.cameraItem.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
 
-    @IBAction func pickAnImage(_ sender: Any) {
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        self.pickAnImage(from: .photoLibrary)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized:
+                self.pickAnImage(from: .camera)
+                
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    if granted {
+                        self.pickAnImage(from: .camera)
+                    }
+                }
+                
+            case .denied:
+                return
+                
+            case .restricted:
+                return
+                
+            @unknown default:
+                return
+        }
+    }
+    
+    private func pickAnImage(from sourceType: UIImagePickerController.SourceType) {
         let pickController = UIImagePickerController()
         pickController.delegate = self
+        pickController.sourceType = sourceType
         present(pickController, animated: true, completion: nil)
     }
     
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
-            imagePickerView.image = image
+            self.imagePickerView.image = image
         }
         dismiss(animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        print("canceled")
         dismiss(animated: true, completion: nil)
     }
     
